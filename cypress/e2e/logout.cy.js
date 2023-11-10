7af67d5bcdb1177c8f879219c59e09e964e9bd44
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-10 13:22:44 +0100

    cypress login and logout tests

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,12 @@
+const validEmail = "pineapple@study.noroff.no";
+const validPassword = "12345678";
+
+describe('logout', () => {
+    it('allows the user to logout with the logout button'), () => {
+        cy.visit('/');
+        cy.get('#loginModal').contains("Logout").click();
+        cy.clearCookies();
+        cy.clearLocalStorage();
+        cy.visit('/');
+    }
+})
