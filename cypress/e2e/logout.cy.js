6225ff67af304dc9e57431b66067980ebb3f8f7b
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-30 00:45:10 +0100

    updates the logout test

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,11 +1,25 @@
-const validEmail = 'pineapple@study.noroff.no';
+const validEmail = 'pineapple@noroff.no';
 const validPassword = '12345678';
 
 describe('logout', () => {
   it('allows the user to logout with the logout button'),
     () => {
       cy.visit('/');
-      cy.get('#loginModal').contains('Logout').click();
+      cy.get('header').within(() => {    
+        cy.get('.text-end').within(() => {
+          cy.get('button.btn.btn-success').click();
+        });
+      });
+      cy.get('#loginEmail').type(validEmail);
+      cy.get('#loginPassword').type(validPassword);
+      cy.get('#loginModal').find('button[type=submit]').click();
+      cy.get('.profile-posts').should('be.visible');
+      cy.get('header').within(() => {    
+        cy.get('.text-end').within(() => {
+          cy.get('button.btn.btn-warning').click();
+        });
+      });
+      cy.get('.profile-posts').should('not.be.visible');
       cy.clearCookies();
       cy.clearLocalStorage();
       cy.visit('/');
