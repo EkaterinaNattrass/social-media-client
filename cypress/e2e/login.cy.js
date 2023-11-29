a6d978b8fa2fec7248632fadf2faa90c1069874e
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-30 00:52:28 +0100

    formats cypress tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,7 +8,7 @@ describe('login', () => {
     cy.visit('/');
   });
   it('logs in and gives access to the valid profile', () => {
-    cy.get('header').within(() => {    
+    cy.get('header').within(() => {
       cy.get('.text-end').within(() => {
         cy.get('button.btn.btn-success').click();
       });
@@ -16,12 +16,14 @@ describe('login', () => {
     cy.get('#loginEmail').type(validEmail);
     cy.get('#loginPassword').type(validPassword);
     cy.get('#loginModal').find('button[type=submit]').click();
-    cy.get('.profile-posts').should('be.visible'); 
-    cy.url().should('include', '?view=profile&name=pineapple', { timeout: 10000 });
+    cy.get('.profile-posts').should('be.visible');
+    cy.url().should('include', '?view=profile&name=pineapple', {
+      timeout: 10000,
+    });
   });
 
   it('denies login and access if the email is invalid', () => {
-    cy.get('header').within(() => {    
+    cy.get('header').within(() => {
       cy.get('.text-end').within(() => {
         cy.get('button.btn.btn-success').click();
       });
@@ -33,7 +35,7 @@ describe('login', () => {
   });
 
   it('denies login and access if the password is invalid', () => {
-    cy.get('header').within(() => {    
+    cy.get('header').within(() => {
       cy.get('.text-end').within(() => {
         cy.get('button.btn.btn-success').click();
       });
@@ -45,7 +47,7 @@ describe('login', () => {
   });
 
   it('denies login and access if the email and password are invalid', () => {
-    cy.get('header').within(() => {    
+    cy.get('header').within(() => {
       cy.get('.text-end').within(() => {
         cy.get('button.btn.btn-success').click();
       });
--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -5,7 +5,7 @@ describe('logout', () => {
   it('allows the user to logout with the logout button'),
     () => {
       cy.visit('/');
-      cy.get('header').within(() => {    
+      cy.get('header').within(() => {
         cy.get('.text-end').within(() => {
           cy.get('button.btn.btn-success').click();
         });
@@ -14,7 +14,7 @@ describe('logout', () => {
       cy.get('#loginPassword').type(validPassword);
       cy.get('#loginModal').find('button[type=submit]').click();
       cy.get('.profile-posts').should('be.visible');
-      cy.get('header').within(() => {    
+      cy.get('header').within(() => {
         cy.get('.text-end').within(() => {
           cy.get('button.btn.btn-warning').click();
         });
