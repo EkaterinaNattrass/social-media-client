c19a8814148db18d2a266d0f1584ea7e01a2b2f3
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-30 09:37:13 +0100

    does formatting

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -2,26 +2,25 @@ const validEmail = 'pineapple@noroff.no';
 const validPassword = '12345678';
 
 describe('logout', () => {
-  it('allows the user to logout with the logout button',
-    () => {
-      cy.visit('/');
-      cy.get('header').within(() => {
-        cy.get('.text-end').within(() => {
-          cy.get('button.btn.btn-success').click();
-        });
+  it('allows the user to logout with the logout button', () => {
+    cy.visit('/');
+    cy.get('header').within(() => {
+      cy.get('.text-end').within(() => {
+        cy.get('button.btn.btn-success').click();
       });
-      cy.get('#loginEmail').type(validEmail);
-      cy.get('#loginPassword').type(validPassword);
-      cy.get('#loginModal').find('button[type=submit]').click();
-      cy.get('.profile-posts').should('be.visible');
-      cy.get('header').within(() => {
-        cy.get('.text-end').within(() => {
-          cy.get('button.btn.btn-outline-warning').click();
-        });
+    });
+    cy.get('#loginEmail').type(validEmail);
+    cy.get('#loginPassword').type(validPassword);
+    cy.get('#loginModal').find('button[type=submit]').click();
+    cy.get('.profile-posts').should('be.visible');
+    cy.get('header').within(() => {
+      cy.get('.text-end').within(() => {
+        cy.get('button.btn.btn-outline-warning').click();
       });
-      cy.get('.profile-posts').should('not.exist');
-      cy.clearCookies();
-      cy.clearLocalStorage();
-      cy.visit('/');
+    });
+    cy.get('.profile-posts').should('not.exist');
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.visit('/');
   });
 });
